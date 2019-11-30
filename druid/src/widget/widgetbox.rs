252dd3586f8d2a58d08c1c2d1cd6086f129f9299
lupyuen <luppy@appkaki.com>
2019-11-30 08:30:20 +0800

    Fixed widgetbox

--- druid/src/widget/widgetbox.rs
+++ druid/src/widget/widgetbox.rs
@@ -5,7 +5,7 @@ use crate::kurbo::{Point, Rect, Size}; ////
 use crate::{
     BaseState, BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx, Widget,
     WidgetPod,
-    widget::Label,
+    widget::{Button, Label},
 };
 
 pub struct WidgetBox<DataType: Data> {
@@ -13,18 +13,33 @@ pub struct WidgetBox<DataType: Data> {
 }
 
 pub enum WidgetType<DataType: Data> {
+    Button(Button<DataType>),
     Label(Label<DataType>),
 }
+
+/*
+impl<DataType: Data> WidgetBox<DataType> {
+    fn get_widget(&mut self) -> &mut impl Widget<DataType> {
+        match &mut self.widget {
+            WidgetType::Button(b) => b,
+            WidgetType::Label(l)  => l,
+        }
+    }
+}
+*/
 
 impl<DataType: Data> Widget<DataType> for WidgetBox<DataType> {
     fn paint(
         &mut self, 
         paint_ctx: &mut PaintCtx, 
-        _base_state: &BaseState, 
+        base_state: &BaseState, 
         data: &DataType, 
         env: &Env
     ) {
-        assert!(false, "no paint");
+        match &mut self.widget {
+            WidgetType::Button(w) => w.paint(paint_ctx, base_state, data, env),
+            WidgetType::Label(w)  => w.paint(paint_ctx, base_state, data, env),
+        };
     }
 
     fn layout(
@@ -34,8 +49,10 @@ impl<DataType: Data> Widget<DataType> for WidgetBox<DataType> {
         data: &DataType,
         env: &Env,
     ) -> Size {
-        assert!(false, "no layout");
-        Size::new(0., 0.)
+        match &mut self.widget {
+            WidgetType::Button(w) => w.layout(layout_ctx, bc, data, env),
+            WidgetType::Label(w)  => w.layout(layout_ctx, bc, data, env),
+        }
     }
 
     fn event(
@@ -45,16 +62,22 @@ impl<DataType: Data> Widget<DataType> for WidgetBox<DataType> {
         data: &mut DataType, 
         env: &Env
     ) {
-        assert!(false, "no event");
+        match &mut self.widget {
+            WidgetType::Button(w) => w.event(ctx, event, data, env),
+            WidgetType::Label(w)  => w.event(ctx, event, data, env),
+        };
     }
 
     fn update(
         &mut self, 
         ctx: &mut UpdateCtx, 
-        _old_data: Option<&DataType>, 
+        old_data: Option<&DataType>, 
         data: &DataType, 
         env: &Env
     ) {
-        assert!(false, "no update");
+        match &mut self.widget {
+            WidgetType::Button(w) => w.update(ctx, old_data, data, env),
+            WidgetType::Label(w)  => w.update(ctx, old_data, data, env),
+        };
     }
 }
