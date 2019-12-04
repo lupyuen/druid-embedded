aa65b7f77843b60303089eeb255d3d8897533c53
lupyuen <luppy@appkaki.com>
2019-12-04 09:18:03 +0800

    Generated dot

--- scripts/gen-call-stack.sh
+++ scripts/gen-call-stack.sh
@@ -3,7 +3,7 @@ rm -f druid/target/thumbv7em-none-eabihf/release/examples/hello*.o
 mv Cargo.toml Cargo.toml.OLD
 cd druid
 
-cargo call-stack -v --target thumbv7em-none-eabihf --example hello
+cargo call-stack -v --target thumbv7em-none-eabihf --example hello >../call-stack.dot
 
 cd ..
 mv Cargo.toml.OLD Cargo.toml
