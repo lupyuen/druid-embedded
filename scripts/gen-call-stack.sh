3e224013c2f05f018eb76d332bc4ba2e20b8e54b
lupyuen <luppy@appkaki.com>
2019-12-04 09:10:58 +0800

    Call stack OK

--- scripts/gen-call-stack.sh
+++ scripts/gen-call-stack.sh
@@ -1,2 +1,12 @@
+rm -f druid/target/thumbv7em-none-eabihf/release/examples/hello*.o
+
+mv Cargo.toml Cargo.toml.OLD
 cd druid
+
 cargo call-stack -v --target thumbv7em-none-eabihf --example hello
+
+cd ..
+mv Cargo.toml.OLD Cargo.toml
+
+arm-none-eabi-readelf --sections druid/target/thumbv7em-none-eabihf/release/examples/hello*.o
+arm-none-eabi-readelf --sections druid/target/thumbv7em-none-eabihf/release/examples/hello
