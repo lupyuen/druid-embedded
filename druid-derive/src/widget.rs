6de452b8035a4ccdfbfa95173d046fb6581bc262
lupyuen <luppy@appkaki.com>
2020-02-14 10:33:15 +0800

    Extended widgets

--- druid-derive/src/widget.rs
+++ druid-derive/src/widget.rs
@@ -1,18 +1,23 @@
 //! Extended implementation for #[derive(Data)] to support static Widgets and Windows on embedded platforms
 
-use quote::quote;
+use quote::{quote, format_ident};
 
 /// Given an Application State type name (e.g. `State`) and struct, derive the static Widgets and Windows
 pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> Result<proc_macro2::TokenStream, syn::Error> {
-    println!("state_type: {:#?}", state_type); ////
-    println!("state_struct: {:#?}", state_struct); ////
+    //println!("state_type: {:#?}", state_type); ////
+    //println!("state_struct: {:#?}", state_struct); ////
+    let data_state = format_ident!("DATA_{}", state_type);
+    let all_widgets_state = format_ident!("ALL_WIDGETS_{}", state_type);
+    let all_windows_state = format_ident!("ALL_WINDOWS_{}", state_type);
+    let all_handlers_state = format_ident!("ALL_HANDLERS_{}", state_type);
+
     let res = quote! {
         use druid_shell::WinHandler;
 
         /// Handle a touch event at the (x,y) coordinates
         pub fn handle_touch(x: u16, y: u16) {
             let mut ctx = druid::DruidContext::new();
-            let handler = unsafe { &mut ALL_HANDLERS_STATE[1] };  //  Assume first window has ID 1
+            let handler = unsafe { &mut #all_handlers_state[1] };  //  Assume first window has ID 1
             handler.mouse_down(
                 &druid_shell::MouseEvent {
                     pos: druid::Point::new(x as f64, y as f64),
@@ -32,51 +37,51 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
         }
         
         /// DATA is the Application Data
-        static mut DATA_STATE: State = State { count: 0 };  //  Generated based on `State`
+        static mut #data_state: #state_type = #state_type { count: 0 };  //  TODO: Generated based on `#state_type`
         
         /// Static list of Widgets for embedded platforms
-        static mut ALL_WIDGETS_STATE: [ druid::WidgetType<State>; druid::MAX_WIDGETS ] = [ 
+        static mut #all_widgets_state: [ druid::WidgetType<#state_type>; druid::MAX_WIDGETS ] = [ 
             druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None,
             druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None, druid::WidgetType::None,
         ];
         
         /// ALL_WINDOWS[i] is the WindowBox for the Window with window ID i. i=0 is not used.
-        static mut ALL_WINDOWS_STATE: [ druid::WindowBox<State>; druid::MAX_WINDOWS ] = [
-            druid::WindowBox::<State>( druid::WindowType::None ), 
-            druid::WindowBox::<State>( druid::WindowType::None ), 
-            druid::WindowBox::<State>( druid::WindowType::None ), 
+        static mut #all_windows_state: [ druid::WindowBox<#state_type>; druid::MAX_WINDOWS ] = [
+            druid::WindowBox::<#state_type>( druid::WindowType::None ), 
+            druid::WindowBox::<#state_type>( druid::WindowType::None ), 
+            druid::WindowBox::<#state_type>( druid::WindowType::None ), 
         ];
         
         /// ALL_HANDLERS[i] is the Window Handler for the Window with window ID i. i=0 is not used.
-        static mut ALL_HANDLERS_STATE: [ druid::DruidHandler<State>; druid::MAX_WINDOWS ] = [
-            druid::DruidHandler::<State> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
-            druid::DruidHandler::<State> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
-            druid::DruidHandler::<State> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
+        static mut #all_handlers_state: [ druid::DruidHandler<#state_type>; druid::MAX_WINDOWS ] = [
+            druid::DruidHandler::<#state_type> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
+            druid::DruidHandler::<#state_type> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
+            druid::DruidHandler::<#state_type> { window_id: druid::WindowId(0), phantom: core::marker::PhantomData },
         ];
         
         /// Specialised Trait to reference Widgets statically on embedded platforms
-        impl druid::GlobalWidgets<State> for druid::WidgetBox<State> {
+        impl druid::GlobalWidgets<#state_type> for druid::WidgetBox<#state_type> {
             /// Fetch the static Widgets for the Data type
-            fn get_widgets(&self) -> &'static mut [ druid::WidgetType<State> ] {
-                unsafe { &mut ALL_WIDGETS_STATE }
+            fn get_widgets(&self) -> &'static mut [ druid::WidgetType<#state_type> ] {
+                unsafe { &mut #all_widgets_state }
             }
             /// Add a Widget for the Data type
-            fn add_widget(&self, widget: druid::WidgetType<State>) {
+            fn add_widget(&self, widget: druid::WidgetType<#state_type>) {
                 assert!(self.0 < druid::MAX_WIDGETS as u32, "too many widgets");
-                unsafe { ALL_WIDGETS_STATE[self.0 as usize] = widget; }        
+                unsafe { #all_widgets_state[self.0 as usize] = widget; }        
             }    
         }
         
         /// Specialised Trait to reference Windows and Window Handlers statically on embedded platforms
-        impl druid::GlobalWindows<State> for druid::AppState<State> {
-            fn add_window(&self, window_id: druid::WindowId, window: druid::WindowBox<State>) {
-                unsafe { ALL_WINDOWS_STATE[window_id.0 as usize] = window; }
+        impl druid::GlobalWindows<#state_type> for druid::AppState<#state_type> {
+            fn add_window(&self, window_id: druid::WindowId, window: druid::WindowBox<#state_type>) {
+                unsafe { #all_windows_state[window_id.0 as usize] = window; }
             }
-            fn add_handler(&self, window_id: druid::WindowId, handler: druid::DruidHandler<State>) {
-                unsafe { ALL_HANDLERS_STATE[window_id.0 as usize] = handler; }
+            fn add_handler(&self, window_id: druid::WindowId, handler: druid::DruidHandler<#state_type>) {
+                unsafe { #all_handlers_state[window_id.0 as usize] = handler; }
             }
-            fn get_handle(&self, window_id: druid::WindowId) -> druid::WindowHandle<druid::DruidHandler<State>> {
-                let handler = unsafe { ALL_HANDLERS_STATE[window_id.0 as usize].clone() };
+            fn get_handle(&self, window_id: druid::WindowId) -> druid::WindowHandle<druid::DruidHandler<#state_type>> {
+                let handler = unsafe { #all_handlers_state[window_id.0 as usize].clone() };
                 druid::WindowHandle(
                     druid::PlatformWindowHandle {
                         window_id: window_id.0,
@@ -87,20 +92,20 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
                     }
                 )
             }
-            fn set_data(&self, data: State) {
-                unsafe { DATA_STATE = data; }
+            fn set_data(&self, data: #state_type) {
+                unsafe { #data_state = data; }
             }
             fn window_event(
                 &mut self, 
                 window_id: druid::WindowId,
-                ctx: &mut druid::EventCtx<State>, 
+                ctx: &mut druid::EventCtx<#state_type>, 
                 event: &druid::Event, 
             ) {
                 unsafe { 
-                    ALL_WINDOWS_STATE[window_id.0 as usize].event(
+                    #all_windows_state[window_id.0 as usize].event(
                         ctx, 
                         event, 
-                        &mut DATA_STATE,  //  Data
+                        &mut #data_state,  //  Data
                         &Env {}           //  Env
                     );
                 }
@@ -108,12 +113,12 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
             fn window_update(
                 &mut self, 
                 window_id: druid::WindowId,
-                ctx: &mut druid::UpdateCtx<State>, 
+                ctx: &mut druid::UpdateCtx<#state_type>, 
             ) {
                 unsafe { 
-                    ALL_WINDOWS_STATE[window_id.0 as usize].update(
+                    #all_windows_state[window_id.0 as usize].update(
                         ctx,
-                        &mut DATA_STATE,  //  Data
+                        &mut #data_state,  //  Data
                         &Env {}           //  Env
                     ); 
                 }
@@ -124,9 +129,9 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
                 layout_ctx: &mut druid::LayoutCtx,
             ) {
                 unsafe { 
-                    ALL_WINDOWS_STATE[window_id.0 as usize].layout(
+                    #all_windows_state[window_id.0 as usize].layout(
                         layout_ctx, 
-                        &mut DATA_STATE,  //  Data
+                        &mut #data_state,  //  Data
                         &Env {}           //  Env
                     ); 
                 }
@@ -137,9 +142,9 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
                 paint_ctx: &mut druid::PaintCtx, 
             ) {
                 unsafe { 
-                    ALL_WINDOWS_STATE[window_id.0 as usize].paint(
+                    #all_windows_state[window_id.0 as usize].paint(
                         paint_ctx, 
-                        &mut DATA_STATE,  //  Data
+                        &mut #data_state,  //  Data
                         &Env {}           //  Env
                     ); 
                 }
@@ -149,7 +154,7 @@ pub fn derive_widget(state_type: syn::Ident, state_struct: syn::DataStruct) -> R
                 window_id: druid::WindowId,
             ) -> bool {
                 unsafe { 
-                    ALL_WINDOWS_STATE[window_id.0 as usize].has_active() 
+                    #all_windows_state[window_id.0 as usize].has_active() 
                 }
             }
         }
