471e44b4ceeea9150bdef8e68e8fdcd4319351b8
lupyuen <luppy@appkaki.com>
2020-02-17 17:03:18 +0800

    Remove external-macro-backtrace

--- scripts/gen-call-stack.cmd
+++ scripts/gen-call-stack.cmd
@@ -5,4 +5,4 @@ cd ..
 arm-none-eabi-readelf --sections druid\target\thumbv7em-none-eabihf\release\examples\hello*.o
 arm-none-eabi-readelf --sections druid\target\thumbv7em-none-eabihf\release\examples\hello
 
-:::: pushd rust/app    ; cargo rustc -v $rust_build_options -- -Z unstable-options --pretty expanded -Z external-macro-backtrace > ../../logs/libapp-expanded.rs    ; popd
+:::: pushd rust/app    ; cargo rustc -v $rust_build_options -- -Z unstable-options --pretty expanded > ../../logs/libapp-expanded.rs    ; popd
